29a3ba9477347d9ce128752e35de1524b10524b8
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2022-07-10 03:50:28 +0530

    add login register product data files and folders

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525085526vxpnm.png",
+      img: "https://o.remove.bg/downloads/e80323a1-ea99-4744-8ea0-5ef3c7154f26/288643142_5015835361862117_7966909598116048651_n-removebg-preview.png",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f8de83",
@@ -11,43 +11,43 @@ export const sliderItems = [
       img: "https://i.ibb.co/DG69bQ4/2.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
+      bg: "faabb9",
     },
     {
       id: 3,
       img: "https://i.ibb.co/cXFnLLV/3.png",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
+      bg: "bfb2f3",
     },
   ];
 
   export const categories = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/282377507_5070059073078507_8905825957736851796_n.jpg?stp=dst-jpg_p960x960&_nc_cat=103&ccb=1-7&_nc_sid=c48759&_nc_ohc=_NvlIvaBIWkAX_5otpP&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT_6oWe2fnPesTz6Os1gGvGtFTqrlhkh-Jt6nEEnWkXq_A&oe=62CE4908",
       title: "SHIRT STYLE!",
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/283493400_7489587521083218_2824616072587987751_n.jpg?stp=dst-jpg_p960x960&_nc_cat=108&ccb=1-7&_nc_sid=c48759&_nc_ohc=8y5-RGrw6FsAX8f2UjJ&tn=1PyL_p61C6NLNxcf&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9ymP37yo1HbXmtrfz0MDocc7nH5DE6IBriZngU9JKU5g&oe=62CEE40D",
+      title: "OFFICE WEAR",
     },
     {
       id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/286409360_5330520680303647_3594611637901910975_n.jpg?stp=dst-jpg_p960x960&_nc_cat=108&ccb=1-7&_nc_sid=c48759&_nc_ohc=MkbUc7VQby4AX9axwzj&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9ZxlL1Ji06SWr_aHXdpj5a8hb-nhYEig451CIQLQ1xHA&oe=62CF10CF",
+      title: "PARTY WEAR",
     },
   ];
 
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img:"https://o.remove.bg/downloads/0c2716a9-2e82-4165-a7cb-36c630c58924/285155784_7711516105556949_752985421388697982_n-removebg-preview.png",
     },
     {
       id:2,
-      img:"https://pngimg.com/uploads/jacket/small/jacket_PNG8039.png",
+      img:"https://o.remove.bg/downloads/fc53ef6a-8fd2-4e30-ab70-29a0db6a65cc/285781736_5490563594307962_5679018170064084979_n-removebg-preview.png",
     },
     {
       id:3,
@@ -55,15 +55,15 @@ export const sliderItems = [
     },
     {
       id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      img:"https://o.remove.bg/downloads/c9702ffd-7117-4ac0-a032-ed272019286d/285571633_5333861050014494_6618049360063942855_n-removebg-preview.png",
     },
     {
       id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      img:"https://o.remove.bg/downloads/b350d36d-08cd-43ce-96c6-78858cd425ad/285944813_7508956432507779_4876775520580441048_n-removebg-preview.png",
     },
     {
       id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      img:"https://o.remove.bg/downloads/02c28c75-70d0-4029-a421-90b5b94c5a17/kisspng-the-dress-clothing-floral-dress-png-transparent-image-5a73faa06d0d99.2197396815175502404467-removebg-preview.png",
     },
     {
       id:7,
---
+++ src/pages/Product.jsx
@@ -0,0 +1,173 @@
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+  
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t1.6435-9/168415473_3687402321372101_9020041914458275069_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=LH8LyJF9_wIAX8Bt4Rp&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9kE3qeEPeVRRoEoewdVrgdgvd3pmKYTZDwan9jKwkJhg&oe=62EFC2B4" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Printed Jump Suit with Bell Sleeves
+            Material - Poly Georgette
+            Style - Bell Sleeves
+            Model Wears - M
+            Brand - S&S
+            Wash & Care - Hand wash with cold water, wash
+            inside out, wash light colours separately & iron
+            with care.
+          </Desc>
+          <Price>Rs. 2510</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
